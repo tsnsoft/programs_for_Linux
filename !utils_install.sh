781585facc0d9a29784baddf711643a39446f6ef
Talipov S.N <proffix4@gmail.com>
2019-11-28 22:11:11 +0600

    Add files via upload

---
+++ !utils_install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt-get install gthumb clementine okular pdfshuffler retext fbreader doublecmd-qt kate qbittorrent okular-extra-backends calibre sigil gparted fslint gsmartcontrol clamtk gtkhash nemo-gtkhash flameshot basket quiterss zeal bleachbit geary
