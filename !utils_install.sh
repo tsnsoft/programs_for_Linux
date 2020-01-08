f083b65db9ea9f0b481f112c3b34d7474d982d76
Talipov S.N <proffix4@gmail.com>
2020-01-08 19:22:48 +0600

    Add files via upload

--- !utils_install.sh
+++ !utils_install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt-get install gthumb clementine okular pdfshuffler retext fbreader doublecmd-qt kate qbittorrent okular-extra-backends calibre sigil gparted fslint gsmartcontrol clamtk gtkhash nemo-gtkhash flameshot basket quiterss zeal bleachbit geary
+sudo apt-get install gthumb clementine okular pdfshuffler retext fbreader doublecmd-qt kate qbittorrent okular-extra-backends calibre sigil gparted fslint gsmartcontrol clamtk gtkhash nemo-gtkhash flameshot basket quiterss zeal bleachbit geary virtualbox-qt virtualbox-ext-pack drawing chromium-browser chromium-browser chromium-codecs-ffmpeg chromium-browser-l10n vlc matplotlib
